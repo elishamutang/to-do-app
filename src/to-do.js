34c8673413a1c76402296277c655107c280eb552
elishamutang <elishamutang@outlook.com>
2024-02-01 08:05:53 +0800

    [refactor] Removed testDialogOpen.

--- src/to-do.js
+++ src/to-do.js
@@ -4,14 +4,13 @@ export function createNewList() {
 
     const testBtn = document.querySelector('.header button');
     const testDialog = document.querySelector('.header dialog');
-    const testDialogOpen = document.querySelector('.header dialog[open]');
 
     testBtn.addEventListener('click', (e) => {
         testDialog.showModal();
     })
 
     testDialog.addEventListener('click', (e) => {
-        if(e.target == testDialog) {
+        if(e.target === testDialog) {
             testDialog.close();
         }
     })
