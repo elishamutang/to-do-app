9785ca597a2a312f12892a242298d49e9e16e8a0
elishamutang <elishamutang@outlook.com>
2024-02-11 15:52:20 +0800

    [feature] Checkboxs and inputs correspond better to each other now.

--- src/allMyTasks.js
+++ src/allMyTasks.js
@@ -4,21 +4,41 @@
 // Add new task and displays them.
 export default function addMyTask(task, fieldset) {
 
+    // Creates new task in overview div.
     const newTaskDiv = document.createElement('div');
     newTaskDiv.className = 'toDoDiv';
 
     const newCheckBox = document.createElement('input');
     newCheckBox.type = 'checkbox';
     newCheckBox.className = 'toDoObj';
-    newCheckBox.id = 'task1';
 
     const newLabel = document.createElement('label');
-    newLabel.htmlFor = 'task1';
     newLabel.textContent = task.value;
+
+    // Check number of To Do's.
+    checkNumOfToDos(newCheckBox, newLabel);
 
     newTaskDiv.append(newCheckBox);
     newTaskDiv.append(newLabel);
 
     fieldset.append(newTaskDiv);
+
+}
+
+function checkNumOfToDos(newCheckBox, newLabel) {
+
+    // Check for number of to do's inside Today fieldset.
+    const existingLables = document.querySelectorAll('#today > div > label');
+    let labelNum = 1;
+
+    // Updates checkbox ID and label for attribute based on number of existing to do's.
+    if(existingLables.length == 0) {
+        newCheckBox.id = `task-${labelNum}`;
+        newLabel.htmlFor = `task-${labelNum}`;
+    } else {
+        const lastLabelNum = existingLables.length;
+        newCheckBox.id = `task-${labelNum+lastLabelNum}`;
+        newLabel.htmlFor = `task-${labelNum+lastLabelNum}`;
+    }
 
 }
