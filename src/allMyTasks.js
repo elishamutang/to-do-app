67fad943dcf0a3aee76a78bf30e65fcc6d9f5c57
elishamutang <elishamutang@outlook.com>
2024-02-11 14:38:24 +0800

    [refactor] Completed this function. Adds new task to the first main div.

--- src/allMyTasks.js
+++ src/allMyTasks.js
@@ -3,25 +3,23 @@
 import { checkFormInput } from "./createNewList";
 
 // Add new task and displays them.
-export default function addMyTask(className, task, fieldset) {
+export default function addMyTask(task, fieldset) {
 
-    checkFormInput(task, className);
+    const newTaskDiv = document.createElement('div');
+    newTaskDiv.className = 'toDoDiv';
 
-    // const newTaskDiv = document.createElement('div');
-    // newTaskDiv.className = 'toDoDiv';
+    const newCheckBox = document.createElement('input');
+    newCheckBox.type = 'checkbox';
+    newCheckBox.className = 'toDoObj';
+    newCheckBox.id = 'task1';
 
-    // const newCheckBox = document.createElement('input');
-    // newCheckBox.type = 'checkbox';
-    // newCheckBox.className = 'toDoObj';
-    // newCheckBox.id = 'task1';
+    const newLabel = document.createElement('label');
+    newLabel.htmlFor = 'task1';
+    newLabel.textContent = task.value;
 
-    // const newLabel = document.createElement('label');
-    // newLabel.htmlFor = 'task1';
-    // newLabel.textContent = task.value;
+    newTaskDiv.append(newCheckBox);
+    newTaskDiv.append(newLabel);
 
-    // newTaskDiv.append(newCheckBox);
-    // newTaskDiv.append(newLabel);
-
-    // fieldset.append(newTaskDiv);
+    fieldset.append(newTaskDiv);
 
 }
