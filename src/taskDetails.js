1342ff09b7e72c4dfce32e76bbdddccdd1476083
elishamutang <elishamutang@outlook.com>
2024-02-16 18:28:42 +0800

    [feature] Created createToDoObjs class and include to-do object specific functions as methods inside the class.

--- src/taskDetails.js
+++ src/taskDetails.js
@@ -3,63 +3,98 @@
 // Target window next to tasks overview (e.g task details).
 const taskDetails = document.getElementById('allTasksDivTwo');
 
-export default function viewTask(task, listOfObjs) {
+// Class
 
-    let currentObj;
+export default class createToDoObj {
 
-    for (const obj of listOfObjs) {
-        if(task.textContent == obj.title) {
-            // Extract title from label
-            currentObj = obj;
-            const getTitle = obj.title;
+    constructor(title, dueDate, priority, notes, checklist) {
+        this.title = title;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.checklist = checklist;
+    };
 
-            // Target header
-            const taskDetailsHeader = document.querySelector('#detailHeader');
-            const taskDetailsHeaderElem = document.createElement('h1');
-            taskDetailsHeaderElem.textContent = getTitle;
+    // Set To-Do object header in taskDetails.
+    viewTask(task, listOfObjs) {
 
-            // Displays to-do detail title.
-            if(taskDetailsHeader.children.length === 0) {
-                taskDetailsHeader.append(taskDetailsHeaderElem);
-            } else {
-                taskDetailsHeader.querySelector('h1').textContent = getTitle;
+        let currentObj;
+
+        for (const obj of listOfObjs) {
+            if(task.textContent == obj.title) {
+
+                // Extract title from label
+                currentObj = obj;
+                const getTitle = obj.title;
+
+                // Target header
+                const taskDetailsHeader = document.querySelector('#detailHeader');
+                const taskDetailsHeaderElem = document.createElement('h1');
+                taskDetailsHeaderElem.textContent = getTitle;
+
+                // Displays to-do detail title.
+                if(taskDetailsHeader.children.length === 0) {
+                    taskDetailsHeader.append(taskDetailsHeaderElem);
+                } else {
+                    taskDetailsHeader.querySelector('h1').textContent = getTitle;
+                }
+
             }
         }
+
+        console.log(currentObj);
     }
 
-    console.log(currentObj);
+    checkList() {
+
+        // Checklist fieldset
+        const checkListFieldset = document.getElementById('checkList');
+
+        // Add input checklist task.
+        const checklistInputDiv = document.createElement('div');
+
+        const checklistInput = document.createElement('input');
+        checklistInput.type = 'checkbox';
+        checklistInput.className = 'toDoObj';
+
+        const checklistInputText = document.createElement('input');
+        checklistInputText.type = 'text';
+        checklistInputText.className = 'taskInputs';
+
+        checklistInputDiv.append(checklistInput);
+        checklistInputDiv.append(checklistInputText);
+
+        checkListFieldset.append(checklistInputDiv);
+
+    }
 
 }
+
 
 // To-Do object checklist
-export function checkList() {
-    
-    // Checklist fieldset
-    const checkListFieldset = document.getElementById('checkList');
+// export function checkList() {
 
-    // Add input checklist task.
-    const checklistInputDiv = document.createElement('div');
+//     // Checklist fieldset
+//     const checkListFieldset = document.getElementById('checkList');
 
-    const checklistInput = document.createElement('input');
-    checklistInput.type = 'checkbox';
-    checklistInput.className = 'toDoObj';
+//     // Add input checklist task.
+//     const checklistInputDiv = document.createElement('div');
 
-    const checklistInputText = document.createElement('input');
-    checklistInputText.type = 'text';
-    checklistInputText.className = 'taskInputs';
+//     const checklistInput = document.createElement('input');
+//     checklistInput.type = 'checkbox';
+//     checklistInput.className = 'toDoObj';
 
-    checklistInputDiv.append(checklistInput);
-    checklistInputDiv.append(checklistInputText);
+//     const checklistInputText = document.createElement('input');
+//     checklistInputText.type = 'text';
+//     checklistInputText.className = 'taskInputs';
 
-    checkListFieldset.append(checklistInputDiv);
+//     checklistInputDiv.append(checklistInput);
+//     checklistInputDiv.append(checklistInputText);
 
-}
+//     checkListFieldset.append(checklistInputDiv);
 
-function checkTaskDetailInputs() {
+// }
 
-
-
-}
 
 export function notes() {
 
