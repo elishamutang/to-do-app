69f2d373334b6da6ce55135a578b9a6957bdae8c
elishamutang <elishamutang@outlook.com>
2024-03-13 23:55:24 +0800

    [feature] Display number of to-do tasks under a specific list/tag.

---
+++ src/updateSidebar.js
@@ -0,0 +1,8 @@
+// Update quantities of to-do task based on tags and list.
+
+export default function updateSidebar(currentObj, listOrTag) {
+
+    console.log(currentObj.list);
+    console.log(listOrTag);
+
+}
