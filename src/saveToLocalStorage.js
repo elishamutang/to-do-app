4e45d854d8e33cb6bef3f8f0ba93fc8072e19b08
elishamutang <elishamutang@outlook.com>
2024-03-18 22:46:26 +0800

    [feature] Added saveUserData function to save user property data.

--- src/saveToLocalStorage.js
+++ src/saveToLocalStorage.js
@@ -3,7 +3,26 @@
 export default function saveToLocal(userData, dataName) {
 
     localStorage.setItem(dataName, JSON.stringify(userData));
-
-    console.log(localStorage);
     
+}
+
+
+export function saveUserData(currentObj, userProp) {
+
+    // Check if listOfObjs exist
+    const listOfObjs = localStorage.getItem("listOfObjs") ? JSON.parse(localStorage.getItem("listOfObjs")) : null;
+
+    for(let list of listOfObjs) {
+                        
+        if(currentObj.title === list.title && currentObj[userProp] !== list[userProp]) {
+
+            console.log(`Old ${userProp}: ${list[userProp]} vs New ${userProp}: ${currentObj[userProp]}`);
+            list[userProp] = currentObj[userProp];
+
+        }
+
+    }
+
+    saveToLocal(listOfObjs, "listOfObjs");
+
 }
