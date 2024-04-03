1c0b41921511ef67ceee387735b8f3ecbd948fdb
elishamutang <elishamutang@outlook.com>
2024-04-04 00:17:55 +0800

    [feature] Populated switchView function.

--- src/switchView.js
+++ src/switchView.js
@@ -1,8 +1,32 @@
 // Switch between different lists and tags.
 
+export default function switchView(selection, homePage) {
 
-export default function switchView() {
+    // Container holding div.overview and addTask form.
+    const allTasksDivOne = document.getElementById('allTasksDivOne');
 
-    // Code goes here
+    if(selection.textContent === "All My Tasks") {
+
+        if(Array.from(allTasksDivOne.children).includes(homePage)) {
+
+            console.log(`div.overview included`);
+
+        } else {
+
+            document.querySelector('.overview').replaceWith(homePage);
+
+        }
+
+    } else {
+
+        // Create a blank page for other pages other than All My Tasks.
+        const otherPage = document.createElement('div');
+        otherPage.className = 'overview';
+        otherPage.className += selection.textContent.includes('#') ? ' ' + selection.textContent.toLowerCase().replace('#', '') : ' ' + selection.textContent.toLowerCase();
+
+        // Replace All My Tasks page with other pages when selected.
+        homePage.replaceWith(otherPage);
+
+    }
 
 }
