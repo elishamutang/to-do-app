c06a85dfef0e1deff6e684fa55209f137d729947
elishamutang <elishamutang@outlook.com>
2024-04-27 16:10:13 +0800

    [refactor] Added CSS minification, prettier and eslint.

--- webpack.common.js
+++ webpack.common.js
@@ -2,51 +2,47 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        assetModuleFilename: '[name][ext]',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist')
-        },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-        watchFiles: ["src/template.html"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.webp$/,
-                type: 'asset/resource',
-                use: ['webp-loader']
-            },
-            {
-                test: /\.(woff|woff2|eot|tff|otf)$/i,
-                type: 'asset/resource'
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'To Do List',
-            filename: 'index.html',
-            template: 'src/template.html',
-            favicon : 'src/assets/favicon/favicon.ico'
-        }),
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+    watchFiles: ['src/template.html'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.webp$/,
+        type: 'asset/resource',
+        use: ['webp-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|tff|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To Do List',
+      filename: 'index.html',
+      template: 'src/template.html',
+      favicon: 'src/assets/favicon/favicon.ico',
+    }),
+  ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,20 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = merge(common ,{
-    mode: 'production',
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: ['...', new CssMinimizerWebpackPlugin()],
+  },
 });
